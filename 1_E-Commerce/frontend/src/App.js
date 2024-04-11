6892d3acee0e499b5ad3307be3444dee61528a3f
rajsharma-sudo <rajparashar931@gmail.com>
2024-04-11 22:09:50 +0530

    more route setup

--- 1_E-Commerce/frontend/src/App.js
+++ 1_E-Commerce/frontend/src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Shop from './Pages/Shop';
-import ShopCategory from './Pages/ShopCategory'
-import Product from './Pages/Product'
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup'
+
+
 function App() {
   return (
     <div>
@@ -17,6 +21,11 @@ function App() {
           <Route path='product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
+
+          {/* More importent route setup */}
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+          
         </Routes>
         
         </BrowserRouter>
