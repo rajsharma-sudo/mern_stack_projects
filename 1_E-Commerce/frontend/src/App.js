5101e354051abb536ca3dce66c384efaee8f4130
rajsharma-sudo <rajparashar931@gmail.com>
2024-04-11 22:04:26 +0530

    We have setup the routes here

--- 1_E-Commerce/frontend/src/App.js
+++ 1_E-Commerce/frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
 function App() {
   return (
     <div>
+        <BrowserRouter>
         <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory category='mens'/>}/>
+          <Route path='/womens' element={<ShopCategory category='womens'/>}/>
+          <Route path='/kids' element={<ShopCategory category='kids'/>}/>
+          <Route path='product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+        </Routes>
+        
+        </BrowserRouter>
     </div>
   );
 }
