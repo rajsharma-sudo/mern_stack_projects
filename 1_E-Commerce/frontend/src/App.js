d8b73978d8aa73a529fe2ac1c42115cc6c700392
rajsharma-sudo <rajparashar931@gmail.com>
2024-04-11 13:57:15 +0530

    some initial updates and changes

--- 1_E-Commerce/frontend/src/App.js
+++ 1_E-Commerce/frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>------Start------</h1>
+    <div>
+        <Navbar/>
     </div>
   );
 }
